3a39b30ea162fc0863a6d9387f3104a397f5cf56
JauniKappauni <social.jauni@gmail.com>
2026-01-22 15:46:29 +0100

    feat: add database manager

--- src/main/java/de/jauni/axbackpacks/AxBackpacks.java
+++ src/main/java/de/jauni/axbackpacks/AxBackpacks.java
@@ -1,13 +1,31 @@
 package de.jauni.axbackpacks;
 
+import de.jauni.axbackpacks.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLException;
+
 public final class AxBackpacks extends JavaPlugin {
+    DatabaseManager databaseManager;
+
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        saveDefaultConfig();
+        try{
+            databaseManager = new DatabaseManager(this);
+            if(databaseManager.initDatabaseTable1() == false){
+                getLogger().severe("Error creating backpacks table.");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
