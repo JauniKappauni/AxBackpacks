8eb375cf0cde85a5fd8b2716c1c6c95ee5ec7157
JauniKappauni <social.jauni@gmail.com>
2026-01-22 09:53:17 +0100

    init: initialized project with basic structure

---
+++ src/main/java/de/jauni/axbackpacks/AxBackpacks.java
@@ -0,0 +1,17 @@
+package de.jauni.axbackpacks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxBackpacks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
